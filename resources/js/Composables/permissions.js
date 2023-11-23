3f03f148937fec219dde395454779898f6bede12
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-11-24 01:19:12 +0600

    5 Create User Resource | Laravel Permission with Inertia

--- resources/js/Composables/permissions.js
+++ resources/js/Composables/permissions.js
@@ -2,7 +2,7 @@ import { usePage } from "@inertiajs/vue3";
 
 export function usePermission() {
     const hasRole = (name) => usePage().props.auth.user.roles.includes(name);
-    const hasPermission = (name) => usePage().props.auth.user.includes(name);
+    const hasPermission = (name) => usePage().props.auth.user.permissions.includes(name);
 
     return { hasRole, hasPermission };
 }

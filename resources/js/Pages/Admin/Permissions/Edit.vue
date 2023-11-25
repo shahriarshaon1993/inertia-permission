fbf77adec2c0705501d5c916311d4c4bb2cac591
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-11-25 23:23:01 +0600

    11 Edit Delete Permissions | Laravel Permission with Inertia

---
+++ resources/js/Pages/Admin/Permissions/Edit.vue
@@ -0,0 +1,54 @@
+<script setup>
+import TextInput from '@/Components/TextInput.vue';
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+
+import { Head, Link, useForm } from '@inertiajs/vue3';
+
+const props = defineProps({
+    permission: {
+        type: Object,
+        required: true
+    }
+});
+
+const form = useForm({
+    name: props.permission?.name
+});
+</script>
+
+<template>
+    <Head title="Update permission" />
+
+    <AdminLayout>
+        <div class="py-4">
+            <div class="flex justify-between items-center">
+                <Link :href="route('permissions.index')"
+                    class="px-3 py-2 text-white font-semibold bg-indigo-500 hover:bg-indigo-700 rounded">
+                Back
+                </Link>
+            </div>
+            <div class="mt-6 max-w-xl mx-auto bg-slate-100 shadow-lg rounded-lg p-6">
+                <h1 class="text-2xl font-semibold uppercase mb-4">Update permission</h1>
+                <form @submit.prevent="form.put(route('permissions.update', permission.id))">
+                    <div>
+                        <InputLabel for="name" value="Name" />
+
+                        <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" autofocus
+                            autocomplete="username" />
+
+                        <InputError class="mt-2" :message="form.errors.name" />
+                    </div>
+
+                    <div class="flex items-center mt-4">
+                        <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            Update
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </AdminLayout>
+</template>
--- resources/js/Pages/Admin/Permissions/Index.vue
+++ resources/js/Pages/Admin/Permissions/Index.vue
@@ -34,7 +34,16 @@ defineProps(['permissions']);
                         <TableRow v-for="(permission, index) in permissions" :key="index" class="border-b">
                             <TableDataCell>{{ permission.id }}</TableDataCell>
                             <TableDataCell>{{ permission.name }}</TableDataCell>
-                            <TableDataCell>Edit/Delete</TableDataCell>
+                            <TableDataCell class="space-x-2">
+                                <Link :href="route('permissions.edit', permission.id)"
+                                    class=" text-green-400 hover:text-green-600">
+                                Edit
+                                </Link>
+                                <Link :href="route('permissions.destroy', permission.id)" method="DELETE" as="button"
+                                    class=" text-red-400 hover:text-red-600">
+                                Delete
+                                </Link>
+                            </TableDataCell>
                         </TableRow>
                     </template>
                 </Table>

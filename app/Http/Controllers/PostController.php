9a93f047e80326bb63c2057c2895c6a995c5f9ba
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-11-28 00:46:03 +0600

    19 Create Post CRUD - Laravel Inertia Permission Tutorial

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreatePostRequest;
 use App\Models\Post;
 use Inertia\Inertia;
 use Inertia\Response;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
+use Illuminate\Http\RedirectResponse;
 
 class PostController extends Controller
 {
@@ -17,5 +19,38 @@ class PostController extends Controller
         return Inertia::render('Admin/Posts/Index', [
             'posts' => PostResource::collection($posts)
         ]);
+    }
+
+    public function create(): Response
+    {
+        return Inertia::render('Admin/Posts/Create');
+    }
+
+    public function store(CreatePostRequest $request): RedirectResponse
+    {
+        Post::create($request->validated());
+
+        return to_route('posts.index');
+    }
+
+    public function edit(Post $post): Response
+    {
+        return Inertia::render('Admin/Posts/Edit', [
+            'post' => new PostResource($post)
+        ]);
+    }
+
+    public function update(CreatePostRequest $request, Post $post): RedirectResponse
+    {
+        $post->update($request->validated());
+
+        return to_route('posts.index');
+    }
+
+    public function destroy(Post $post): RedirectResponse
+    {
+        $post->delete();
+
+        return back();
     }
 }
--- resources/js/Pages/Admin/Posts/Create.vue
+++ resources/js/Pages/Admin/Posts/Create.vue
@@ -4,47 +4,35 @@ import AdminLayout from '@/Layouts/AdminLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import VueMultiselect from 'vue-multiselect';
 
 import { Head, Link, useForm } from '@inertiajs/vue3';
 
-defineProps({
-    permissions: Array
-});
-
 const form = useForm({
-    name: "",
-    permissions: []
+    title: "",
 });
 </script>
 
 <template>
-    <Head title="Create new role" />
+    <Head title="Create new post" />
 
     <AdminLayout>
         <div class="py-4">
             <div class="flex justify-between items-center">
-                <Link :href="route('roles.index')"
+                <Link :href="route('posts.index')"
                     class="px-3 py-2 text-white font-semibold bg-indigo-500 hover:bg-indigo-700 rounded">
                 Back
                 </Link>
             </div>
             <div class="mt-6 max-w-6xl mx-auto bg-slate-100 shadow-lg rounded-lg p-6">
-                <h1 class="text-2xl font-semibold uppercase mb-4">Create new role</h1>
-                <form @submit.prevent="form.post(route('roles.store'))">
+                <h1 class="text-2xl font-semibold uppercase mb-4">Create new post</h1>
+                <form @submit.prevent="form.post(route('posts.store'))">
                     <div class="mt-4">
-                        <InputLabel for="name" value="Name" />
+                        <InputLabel for="title" value="Title" />
 
-                        <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" autofocus
-                            autocomplete="username" />
+                        <TextInput id="title" type="text" class="mt-1 block w-full" v-model="form.title" autofocus
+                            autocomplete="title" />
 
-                        <InputError class="mt-2" :message="form.errors.name" />
-                    </div>
-
-                    <div class="mt-4">
-                        <InputLabel for="permissions" value="Permissions" />
-                        <VueMultiselect v-model="form.permissions" :options="permissions" :multiple="true"
-                            :close-on-select="true" placeholder="Pick some" label="name" track-by="id" />
+                        <InputError class="mt-2" :message="form.errors.title" />
                     </div>
 
                     <div class="flex items-center mt-4">
@@ -57,4 +45,3 @@ const form = useForm({
         </div>
     </AdminLayout>
 </template>
-<style src="vue-multiselect/dist/vue-multiselect.css"></style>

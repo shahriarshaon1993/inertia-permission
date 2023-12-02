6ae64b054b14478c03300b83d9a77d975b426c41
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-12-02 22:37:51 +0600

    21 Show Links based On Permissions - Laravel Inertia Permissions Tutorial

--- resources/js/Pages/Admin/Posts/Index.vue
+++ resources/js/Pages/Admin/Posts/Index.vue
@@ -8,12 +8,15 @@ import Modal from '@/Components/Modal.vue';
 import DangerButton from '@/Components/DangerButton.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
 
+import { usePermission } from '@/Composables/permissions';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 import { ref } from 'vue';
 
 defineProps(['posts']);
 
 const showConfirmDeletePostModal = ref(false);
+
+const { hasPermission } = usePermission();
 
 const form = useForm({});
 
@@ -39,10 +42,13 @@ const deletePost = (id) => {
         <div class="py-4">
             <div class="flex justify-between items-center">
                 <h1>Welcome, Posts</h1>
-                <Link :href="route('posts.create')"
-                    class="px-3 py-2 text-white font-semibold bg-indigo-500 hover:bg-indigo-700 rounded">
-                New Post
-                </Link>
+
+                <template v-if="hasPermission('create post')">
+                    <Link :href="route('posts.create')"
+                        class="px-3 py-2 text-white font-semibold bg-indigo-500 hover:bg-indigo-700 rounded">
+                    New Post
+                    </Link>
+                </template>
             </div>
             <div class="mt-6">
                 <Table>
@@ -57,12 +63,17 @@ const deletePost = (id) => {
                             <TableDataCell>{{ post.id }}</TableDataCell>
                             <TableDataCell>{{ post.title }}</TableDataCell>
                             <TableDataCell class="space-x-2">
-                                <Link :href="route('posts.edit', post.id)" class=" text-green-400 hover:text-green-600">
-                                Edit
-                                </Link>
-                                <button @click="confirmDeletePost" class=" text-red-400 hover:text-red-600">
-                                    Delete
-                                </button>
+                                <template v-if="hasPermission('update post')">
+                                    <Link :href="route('posts.edit', post.id)" class=" text-green-400 hover:text-green-600">
+                                    Edit
+                                    </Link>
+                                </template>
+
+                                <template v-if="hasPermission('delete post')">
+                                    <button @click="confirmDeletePost" class=" text-red-400 hover:text-red-600">
+                                        Delete
+                                    </button>
+                                </template>
 
                                 <!-- Confirm Modal -->
                                 <Modal :show="showConfirmDeletePostModal" @click="closeModal">
